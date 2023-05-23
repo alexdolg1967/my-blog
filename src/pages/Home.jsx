76662433513a2cc9545f850e1fe31fe0b5d26894
alexdolg1967 <alexdolg1967@mail.ru>
2023-05-23 14:43:47 +0300

    add pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,22 +7,22 @@ const Home = () => {
     <>
       <Header />
 
-<main className="section">
-  <div className="container">
+      <main className="section">
+        <div className="container">
 
-    <ul className="content-list">
-      <li className="content-list__item">
-        <h2 className="title-2">Frontend</h2>
-        <p>JavaScript, TypeScript, ReactJS, Redux, HTML, CSS, NPM, BootStrap, MaterialUI, TailwindCSS, StyledComponents</p>
-      </li>
-      <li className="content-list__item">
-        <h2 className="title-2">Backend</h2>
-        <p>NodeJS, MySQL, MongoDB, PHP, Laravel</p>
-      </li>
-    </ul>
+          <ul className="content-list">
+            <li className="content-list__item">
+              <h2 className="title-2">Frontend</h2>
+              <p>JavaScript, TypeScript, ReactJS, Redux, HTML, CSS, NPM, BootStrap, MaterialUI, TailwindCSS, StyledComponents</p>
+            </li>
+            <li className="content-list__item">
+              <h2 className="title-2">Backend</h2>
+              <p>NodeJS, MySQL, MongoDB, PHP, Laravel</p>
+            </li>
+          </ul>
 
-  </div>
-</main>
+        </div>
+      </main>
     </>
   )
 }
---
+++ src/pages/Projects.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+
+import project1 from '../img/projects/01.jpg'
+import project2 from '../img/projects/02.jpg'
+import project3 from '../img/projects/03.jpg'
+import project4 from '../img/projects/04.jpg'
+import project5 from '../img/projects/05.jpg'
+import project6 from '../img/projects/06.jpg'
+
+
+const Projects = () => {
+  return (
+    <main className="section">
+        <div className="container">
+            <h2 className="title-1">Projects</h2>
+            <ul className="projects">
+                <li className="project">
+                    <a href="./project-page.html">
+                        <img src={project1} alt="Project img" className="project__img" />
+                        <h3 className="project__title">Gaming streaming portal</h3>
+                    </a>
+                </li>
+                <li className="project">
+                    <a href="./project-page.html">
+                        <img src={project2} alt="Project img" className="project__img" />
+                        <h3 className="project__title">Video service</h3>
+                    </a>
+                </li>
+                <li className="project">
+                    <a href="./project-page.html">
+                        <img src={project3} alt="Project img" className="project__img" />
+                        <h3 className="project__title">Video portal</h3>
+                    </a>
+                </li>
+                <li className="project">
+                    <img src={project4} alt="Project img" className="project__img" />
+                    <h3 className="project__title">Dating app</h3>
+                </li>
+                <li className="project">
+                    <img src={project5} alt="Project img" className="project__img" />
+                    <h3 className="project__title">Landing</h3>
+                </li>
+                <li className="project">
+                    <img src={project6} alt="Project img" className="project__img" />
+                    <h3 className="project__title">Gaming community</h3>
+                </li>
+
+            </ul>
+        </div>
+    </main>
+  )
+}
+
+export default Projects
